8acfe1e0fcadbb6ab0b16b7c612220ad18f099c9
Salum28 <willmanzur14@gmail.com>
2023-10-09 20:37:47 -0300

    Alert Dialog Corrigido

--- lib/home.dart
+++ lib/home.dart
@@ -39,6 +39,7 @@ class _HomeState extends State<Home> {
     }
     showDialog(
       context: context,
+      barrierDismissible: false,
       builder: (context) {
         return AlertDialog(
           title: Text(_alertTitle),
